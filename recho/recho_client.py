11e60716adf93159e9d016524eb1dab854710e33
Bob Arima <rtarima@yahoo.com>
2012-01-10 20:52:58 -0800

    This is the updated entry for the lab

--- recho/recho_client.py
+++ recho/recho_client.py
@@ -21,12 +21,15 @@ if nargs > 1:
 if nargs > 2:
     port = int(sys.argv[2])
 
-s = socket.socket(socket.AF_INET, 
-                  socket.SOCK_STREAM) 
-s.connect((host,port)) 
-string_to_send = raw_input('-> ')
-s.send(string_to_send) 
-data = s.recv(size) 
-s.close() 
-print 'from (%s,%s) %s' % (host, port, data)
+while True:
+	string_to_send = raw_input('-> ')
+	if (string_to_send == ''):
+		s.close()
+		break;
+	else:
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+		s.connect((host,port)) 
+		s.send(string_to_send) 
+		data = s.recv(size) 
+		s.close() 
 
