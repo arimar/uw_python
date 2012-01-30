886f87a7401d4894dd5186cef3508d858c0b11ad
Bob Arima <rtarima@yahoo.com>
2012-01-29 21:27:06 -0800

    Push chat up to master

---
+++ chat/chat_client.py
@@ -0,0 +1,50 @@
+"""
+echo client, usage:
+
+ python echo_client.py <host> <port>
+
+Both host and port are optional, defaults: localhost 50000
+host must be present if you want to provide port
+"""
+
+import socket 
+import sys
+import select
+
+host = 'localhost' 
+port = 50003 
+size = 1024 
+timeout = 10
+
+nargs = len(sys.argv)
+if nargs > 1:
+    host = sys.argv[1]
+if nargs > 2:
+    port = int(sys.argv[2])
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect((host,port))
+
+print 'Connection accepted by (%s,%s)' % (host,port)
+
+while True:
+    try:
+	inputready, outputready,exceptready = select.select([0,s],[],[])
+
+	for i in inputready:
+		if i == 0:
+			data = sys.stdin.readline().strip()
+	        	if data: s.send(s,data)
+		else:
+	        	data = s.recv(s)
+	        	if not data:
+	        		print 'Shutting down.'
+		        	break
+                	elif s:
+                        	sys.stdout.write(data + '\n')
+                        	sys.stdout.flush()
+
+    except KeyboardInterrupt:
+        print 'Interrupted.'
+	s.close()
+	break
