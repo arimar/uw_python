3f1fec7f3228d46786b8c9de9a443422b723a3ce
Bob Arima <rtarima@yahoo.com>
2012-02-26 21:49:56 -0800

    Added wsdot.py and sunrise_sunset.py

---
+++ scrape/sunrise_sunset.py
@@ -0,0 +1,20 @@
+import urllib2
+from BeautifulSoup import BeautifulSoup
+
+#proxy = urllib2.ProxyHandler({'http' : 'http://www-blv-proxy.boeing.com:31060'})
+#opener = urllib2.build_opener(proxy)
+#urllib2.install_opener(opener)
+
+page = urllib2.urlopen('http://www.timeanddate.com/worldclock/astronomy.html?n=234')
+
+soup = BeautifulSoup(page)
+
+#
+# Note:  <table class="spad">
+# <table class="spad">
+# TABLE IS FOUND FIRST
+#
+for row in soup('table', {'class' : 'spad'})[0].tbody('tr'):
+    tds = row('td')
+    print tds[0].string, ' Sunrise: ', tds[1].string, ' Sunset: ', tds[2].string
+    # will print date, sunrise, and sunset
---
+++ scrape/wsdot.py
@@ -0,0 +1,69 @@
+import urllib2
+from BeautifulSoup import BeautifulSoup
+
+#proxy = urllib2.ProxyHandler({'http' : 'http://www-blv-proxy.boeing.com:31060'})
+#opener = urllib2.build_opener(proxy)
+#urllib2.install_opener(opener)
+
+page_north = urllib2.urlopen('http://www.wsdot.wa.gov/small/Seattle/TravelTimes/nbtimes.aspx')
+page_south = urllib2.urlopen('http://www.wsdot.wa.gov/small/Seattle/TravelTimes/sbtimes.aspx')
+
+soup = BeautifulSoup(page_north)
+
+print "*****************************************************************"
+print "                          HEADING NORTH"
+print "*****************************************************************"
+
+try:
+    for row in soup('tr', {'class' : 'tablerow'}):
+    	tds = row('td')
+    	#print [tds[0].findAll(text=True).pop(i) for i in range(len(tds[0].findAll(text=True)))]
+    	for i in range(len(tds[0].findAll(text=True))):
+        	print tds[0].findAll(text=True).pop(i),
+    		print
+    	print "    Average:  %s minutes, " % tds[2].text,
+    	print "Current:  %s minutes" % tds[3].text
+    	print
+
+    for row in soup('tr', {'class' : 'tablerowalt'}):
+    	tds = row('td')
+    	#print [tds[0].findAll(text=True).pop(i) for i in range(len(tds[0].findAll(text=True)))]
+    	for i in range(len(tds[0].findAll(text=True))):
+        	print tds[0].findAll(text=True).pop(i),
+    	print
+    	print "    Average:  %s minutes, " % tds[2].text,
+    	print "Current:  %s minutes" % tds[3].text
+    	print
+
+except IndexError:
+    pass
+
+soup = BeautifulSoup(page_south)
+
+print "*****************************************************************"
+print "                          HEADING SOUTH"
+print "*****************************************************************"
+
+try:
+    for row in soup('tr', {'class' : 'tablerow'}):
+    	tds = row('td')
+    	#print [tds[0].findAll(text=True).pop(i) for i in range(len(tds[0].findAll(text=True)))]
+    	for i in range(len(tds[0].findAll(text=True))):
+        	print tds[0].findAll(text=True).pop(i),
+    	print
+    	print "    Average:  %s minutes, " % tds[2].text,
+    	print "Current:  %s minutes" % tds[3].text
+    	print
+
+    for row in soup('tr', {'class' : 'tablerowalt'}):
+    	tds = row('td')
+    	#print [tds[0].findAll(text=True).pop(i) for i in range(len(tds[0].findAll(text=True)))]
+    	for i in range(len(tds[0].findAll(text=True))):
+        	print tds[0].findAll(text=True).pop(i),
+    	print
+    	print "    Average:  %s minutes, " % tds[2].text,
+    	print "Current:  %s minutes" % tds[3].text
+    	print
+
+except IndexError:
+    pass
